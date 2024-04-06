ed243f5f91c70eec38bbca11e4cccef9d95ac562
andre.guterres <andre.lepesteur@gmail.com>
2024-04-05 21:54:23 -0300

    Parcial Dtos

---
+++ ProjetoDomain/Dtos/ClienteDto.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDomain.Dtos
+{
+    public class ClienteDto
+    {
+        public int ClienteId { get; set; }
+        public string? Email { get; set; }
+        public string? Logotipo { get; set; }
+        public List<LogradouroDto> Logradouros { get; set; } = new List<LogradouroDto>();
+    }
+}
---
+++ ProjetoDomain/Dtos/LogradouroDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDomain.Dtos
+{
+    public class LogradouroDto
+    {
+        public int LogradouroId { get; set; }
+        public string? NomeRua { get; set; }
+        public int Numero { get; set; }
+        public string? Bairro { get; set; }
+    }
+}

64cd1c275f94369eaef02bf05beeedb39e46e6b2
andre.guterres <andre.lepesteur@gmail.com>
2024-04-05 21:52:14 -0300

    Parcial das entidades

---
+++ ProjetoDomain/Entities/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDomain.Entities
+{
+    public class Cliente
+    {
+        //Nome, e-mail, Logotipo* e Logradouro
+        public int ClienteId { get; set; }
+        public string? Email { get; set; }
+        public string? Logotipo { get; set; }
+        public List <Logradouro> Logradouros { get; set; } = new List<Logradouro>();
+    }
+}
---
+++ ProjetoDomain/Entities/Logradouro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDomain.Entities
+{
+    public class Logradouro
+    {
+        public int LogradouroId { get; set; }
+        public string? NomeRua { get; set; }
+        public int Numero { get; set; }
+        public string? Bairro { get; set; }
+
+    }
+}
